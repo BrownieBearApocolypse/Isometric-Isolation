0a28be704477e24507d79a01bfb3a2b698edc109
Maddy Ellicott <maddyellicott195@gmail.com>
2022-12-08 10:15:13 +1100

    movement fixed

--- Assets/scripts/IsoMovement.cs
+++ Assets/scripts/IsoMovement.cs
@@ -3,23 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class charctermover : MonoBehaviour
+
+public class IsoMovement : MonoBehaviour
 {
     [SerializeField]
     public float moveSpeed = 4f;
-
-
     Vector3 forward, right;
-
-    //bool jump = false;
-   // public float jumpHeight = 8f;
-    //public float jumpSpeed = 10f;
     Rigidbody rb;
+    public Vector3 jump;
+    public float jumpForce = 2.0f;
+    public bool isGrounded;
+       
+
+    void OnCollisionStay()
+    {
+        isGrounded = true;
+    }    
 
     // Use this for initialization
     void Start()
     {
-
         forward = Camera.main.transform.forward;
         forward.y = 0;
         forward = Vector3.Normalize(forward);
@@ -27,37 +30,36 @@ public class charctermover : MonoBehaviour
         // -45 degrees from the world x axis
         right = Quaternion.Euler(new Vector3(0, 90, 0)) * forward;
         rb = GetComponent<Rigidbody>();
-
+        jump = new Vector3(0.0f, 2.0f, 0.0f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Jump"))
-        {
-            Jump();
-        }
-
-        else
-        {
-            Move();
-        }
+        Move();
+       
 
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             SceneManager.LoadScene(0);
         }
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+
+            rb.AddForce(jump * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
     }
 
     void Move()
     {
-        
         // Movement speed
         Vector3 rightMovement = right * moveSpeed * Input.GetAxisRaw("Horizontal");
         Vector3 upMovement = forward * moveSpeed * Input.GetAxisRaw("Vertical");
         float HAX = Input.GetAxisRaw("Horizontal");
         float VAX = Input.GetAxisRaw("Vertical");
-        Debug.Log("Hax: " + HAX + " VAX: " + VAX);
+       
 
         // Calculate what is forward
         Vector3 heading = Vector3.Normalize(rightMovement + upMovement);
@@ -72,16 +74,13 @@ public class charctermover : MonoBehaviour
         // Smoothly move the new position
         transform.forward = heading;
         transform.position = Vector3.Lerp(transform.position, newPosition, Time.deltaTime);
-        
+
         rightMovement = Vector3.zero;
         upMovement = Vector3.zero;
-
-
+        
     }
 
-    void Jump()
-    {
-
-    }
     
 }
+
+
